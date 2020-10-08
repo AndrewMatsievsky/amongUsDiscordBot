0caab400344669dc5fed937f6fb31a6b75b66ea6
Andrii Matsiievskyi <amatsiievskyi@cybercraftinc.com>
2020-10-08 09:07:31 +0300

    Few fixes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const config = require("./config.json");
 const client = new Discord.Client();
 const fs = require('fs');
 
+const WEBHOOK_BOT_ID ='760964374654746646'
+const WEBHOOK_READY_VOICE_ID = '659088565103493151'
+
 const prefix = "/";
 
 client.on("message", function(message) {
@@ -13,26 +16,26 @@ client.on("message", function(message) {
   const command = args.shift().toLowerCase();
 
   if (command === "mute") {
-      message.channel.send('SHHHHHHH!')
+    message.channel.send('SHHHHHHH!')
 
-      const channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+    const channel = message.guild.channels.cache.get(message.author.bot && message.author.id === WEBHOOK_BOT_ID ? WEBHOOK_READY_VOICE_ID : message.member.voice.channel.id);
 
-      for (const [memberID, member] of channel.members) {
-        if (member.user.username !== 'MuteVoiceForEveryOne') {
-          member.voice.setMute(true);
-        }
+    for (const [memberID, member] of channel.members) {
+      if (member.user.username !== 'MuteVoiceForEveryOne') {
+        member.voice.setMute(true);
       }
+    }
 
-      channel.join().then(connection => {
-        connection.play(fs.createReadStream('./airplane_sound.opus'), {
-          type: 'ogg/opus',
-        })
+    channel.join().then(connection => {
+      connection.play(fs.createReadStream('./airplane_sound.opus'), {
+        type: 'ogg/opus',
       })
+    })
   }
 
   if (command === 'talk') {
     message.channel.send('You can talk now!')
-    const channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+    const channel = message.guild.channels.cache.get(message.author.bot && message.author.id === WEBHOOK_BOT_ID ? WEBHOOK_READY_VOICE_ID : message.member.voice.channel.id);
 
     for (const [memberID, member] of channel.members) {
       member.voice.setMute(false);
